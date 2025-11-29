97417068c457dd7fdd715b68cd20404e69c4dbee
Mateus <gianmateus22@icloud.com>
2025-11-29 11:45:53 +0100

    feat: Add VS Code C/C++ IntelliSense, build, and debug configurations.

--- c/operadoresLógicos/n2r_nc1_2.c
+++ c/operadoresLógicos/n2r_nc1_2.c
@@ -1,3 +1,17 @@
 //
 // Created by Mateus e Maria on 29.11.25.
 //
+
+#include <stdio.h>
+
+int main() {
+
+    int a = 10;
+
+    if (!(a > 0)) {
+        printf("A variável é positiva.\n");
+    } else {
+        printf("A variável é negativa.\n");
+    }
+
+}
