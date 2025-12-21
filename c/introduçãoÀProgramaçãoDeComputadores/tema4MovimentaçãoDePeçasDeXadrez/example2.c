34996cf443f37134583e09997313cb4018fee8db
Mateus <gianmateus22@icloud.com>
2025-12-21 10:15:47 +0100

    chore: remove a file

--- c/introduçãoÀProgramaçãoDeComputadores/tema4MovimentaçãoDePeçasDeXadrez/example2.c
+++ c/introduçãoÀProgramaçãoDeComputadores/tema4MovimentaçãoDePeçasDeXadrez/example2.c
@@ -1,3 +1,15 @@
-//
-// Created by Mateus e Maria on 19.12.25.
-//
+#include <stdio.h>
+
+int main () {
+
+    int numero, i;
+
+    printf("Digite um número para calcularmos a tabuada... \n");
+    scanf("%d", &numero);
+
+    for (i = 0; i <= 10; i++){
+        printf("%d x %d = %d\n", i, numero, i * numero);
+    }
+
+    return 0;
+}
