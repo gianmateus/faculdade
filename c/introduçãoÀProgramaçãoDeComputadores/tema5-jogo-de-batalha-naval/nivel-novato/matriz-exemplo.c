d69d99c3744c083f54a920028c8668278cb456a8
Mateus Marques <gianmateus22@icloud.com>
2026-01-06 10:22:00 +0100

    exemplo matriz

---
+++ c/introduçãoÀProgramaçãoDeComputadores/tema5-jogo-de-batalha-naval/nivel-novato/matriz-exemplo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+  printf("O elemento na posição [0][0] %d\n", matriz[0][0]);
+  printf("O elemento na posição [1][1] %d\n", matriz[1][1]);
+  printf("O elemento na posição [2][2] %d\n", matriz[2][2]);
+
+  return 0;
+}
